21ae1ba132f83c174f204d3f8a8ef5fff1b3dc5b
Vasil Kotsev <vasil.kotsev@gmail.com>
2021-07-30 18:37:06 +0300

    adding sample vignette card

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
-  $(".vignette").on("change", function (e) {
-    e.preventDefault();
-    let label = $(this).find(".vignette-note-label");
-    let input = $(this).find(".vignette-note");
-    $(label).toggleClass("shrink");
-    console.log(input.val());
-  });
+  // $(".vignette").on("change", function (e) {
+  //   e.preventDefault();
+  //   let label = $(this).find(".vignette-note-label");
+  //   let input = $(this).find(".vignette-note");
+  //   $(label).toggleClass("shrink");
+  //   console.log(input.val());
+  // });
 });
