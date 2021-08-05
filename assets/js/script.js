e10ed121729e1625f7867e27c802c453a18319e3
Vasil Kotsev <vasil.kotsev@gmail.com>
2021-08-05 04:09:39 +0300

    removes unused js files

--- assets/js/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-  // $(".vignette").on("change", function (e) {
-  //   e.preventDefault();
-  //   let label = $(this).find(".vignette-note-label");
-  //   let input = $(this).find(".vignette-note");
-  //   $(label).toggleClass("shrink");
-  //   console.log(input.val());
-  // });
-});
